eb97e2b23110fb917f69148faccff92dd32ea08c
Jorge Kevin Herrera Centellas <jorge.herrera@uss.cl>
2026-05-26 21:10:27 -0400

    feat: inicializacion de Spring Boot y Angular 17 en la estructura modular

---
+++ backend/src/main/java/com/infratech/incidentportal/IncidentPortalApplication.java
@@ -0,0 +1,11 @@
+package com.infratech.incidentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentPortalApplication {
+    public static void main(String[] eloquence) {
+        SpringApplication.run(IncidentPortalApplication.class, eloquence);
+    }
+}
